a31f1b753579d8ef038c2a88d9c0b94d4b4d553c
zdurr22 <zdurr22@gmail.com>
2021-09-25 15:42:10 -0400

    commit tree a727f11e6725d0702a3bab61e8823889b09d7076

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
